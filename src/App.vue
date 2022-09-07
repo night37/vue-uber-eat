81af1ce62e44e4794393b10d617018ad65267186
Garnier <garnier.guillaume37550@gmail.com>
2022-09-07 19:38:45 +0200

    vue init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <RestaurantCard/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RestaurantCard from "./components/RestaurantCard.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    RestaurantCard
+  },
+};
 </script>
 
 <style>
+@import "./style/reset.scss";
+@import url("https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="restaurant-card">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quaerat harum porro exercitationem fugiat distinctio velit voluptas! Doloremque hic expedita optio neque ducimus dolor voluptas dolorum obcaecati harum, laborum incidunt.</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+.restaurant-card{
+    width: 32%;
+    height: 30vh;
+}
+
+</style>
